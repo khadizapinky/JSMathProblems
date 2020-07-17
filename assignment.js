cc93aa0ddddcdbe988a7ffdd2596616e7001edd1
khadizapinky <khadizapinky303@gmail.com>
2020-07-17 21:43:25 +0600

    some changes

--- assignment.js
+++ assignment.js
@@ -1,8 +1,13 @@
 //feetToMile
 function feetToMile(feet){
-    var mile = feet/ 5280; 
-    return mile;
-}
+    var mile = feet / 5280; 
+    if(feet < 0){
+        return "invalid input!";
+        }
+    else{
+          return mile;
+        }
+    }
 var mileCalculation = feetToMile(30000);
 console.log(mileCalculation);
 
@@ -14,7 +19,12 @@ function woodCalculator(chair, table, bed){
     var tableWood = table * 3;
     var bedWood = bed * 5;
     var totalWood = chairWood + tableWood + bedWood;
-    return totalWood;
+    if(totalWood < 0){
+        return "invalid input!";
+    }
+    else{
+       return totalWood;
+    }
 }
  var totalFurnitureWood = woodCalculator(6, 3, 3);
  console.log(totalFurnitureWood);
@@ -30,24 +40,42 @@ function brickCalculator(floor){
     if(floor <= 10){
      var firstTenBricks = 1000* 15;
      var totalBricks  = firstTenBricks * floor;
-     return totalBricks;
-    }
+
+     if(totalBricks < 0){
+         return "invalid input!";
+     }
+     else{
+         return totalBricks;
+     }
+}
     else if(floor <= 20 && floor > 10){
       var firstTenBricks = 1000 * 15 * 10;
       var secondTenBricks = 1000 * 12 * (floor-10);
       var totalBricks = firstTenBricks + secondTenBricks;
+
+      if(totalBricks < 0){
+        return "invalid input!";
+      }
+      else{
       return totalBricks;
-    }
+      }
+}
      else{
       var firstTenBricks = 1000 * 15 * 10;
       var secondTenBricks = 1000 * 12 * 10;
       var thirdTenBricks = 1000 * 10 * (floor - 20);
       var totalBricks = firstTenBricks + secondTenBricks + thirdTenBricks;
+
+      if(totalBricks < 0){
+        return "invalid input!";
+      }
+      else{
       return totalBricks;
-    }
+      }
+ }
     
 }
-var totalFloorBrick = brickCalculator(15);
+var totalFloorBrick = brickCalculator(25);
 console.log(totalFloorBrick);
 
 
@@ -67,6 +95,6 @@ function  tinyFriend(names){
     }
     return shortestName;
 }
-var shortestNameFind = tinyFriend(['kalam', 'salman',  'ashmaa', 'diya']);
+var shortestNameFind = tinyFriend(['kalam', 'sn',  'ashmaa', 'diya']);
 console.log(shortestNameFind);
 
