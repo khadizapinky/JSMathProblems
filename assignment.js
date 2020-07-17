5e76ba5ac14cb7a904945878d5504d7afe540d1e
khadizapinky <khadizapinky303@gmail.com>
2020-07-17 20:40:13 +0600

    again commit

--- assignment.js
+++ assignment.js
@@ -3,23 +3,21 @@ function feetToMile(feet){
     var mile = feet/ 5280; 
     return mile;
 }
-var roadfeet = feetToMile(30000);
-console.log(roadfeet);
-
-
+var mileCalculation = feetToMile(30000);
+console.log(mileCalculation);
 
 
 
 //woodCalculator
-// function woodCalculator(chair, table, bed){
-//     var chairWood = chair * 1;
-//     var tableWood = table * 3;
-//     var bedWood = bed * 5;
-//     var totalWood = chairWood + tableWood + bedWood;
-//     return totalWood;
-// }
-//  var result = woodCalculator(2, 2, 2);
-//  console.log(result);
+function woodCalculator(chair, table, bed){
+    var chairWood = chair * 1;
+    var tableWood = table * 3;
+    var bedWood = bed * 5;
+    var totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+ var totalFurnitureWood = woodCalculator(6, 3, 3);
+ console.log(totalFurnitureWood);
 
 
 
@@ -27,22 +25,30 @@ console.log(roadfeet);
 
 
 //brickCalculator
-function brickCalculator(numbers){
-    var oneFeetBrick = 1000;
-    var totalBrick = 0;
-    if(numbers <= 10){
-      totalBrick = oneFeetBrick * 15;
+function brickCalculator(floor){
+   
+    if(floor <= 10){
+     var FirstTenBricks = 1000* 15;
+     var totalBricks  = FirstTenBricks * floor;
+     return totalBricks;
     }
-    else if(numbers <= 20 && numbers > 10){
-       totalBrick = oneFeetBrick * 12;
+    else if(floor <= 20 && floor > 10){
+      var FirstTenBricks = 1000 * 15 * 10;
+      var secondTenBricks = 1000 * 12 * (floor-10);
+      var totalBricks = FirstTenBricks + secondTenBricks;
+      return totalBricks;
     }
-    else{
-       totalBrick = oneFeetBrick * 10;
+     else{
+      var FirstTenBricks = 1000 * 15 * 10;
+      var secondTenBricks = 1000 * 12 * 10;
+      var ThirdTenBricks = 1000 * 10 * (floor - 20);
+      var totalBricks = FirstTenBricks + secondTenBricks + ThirdTenBricks;
+      return totalBricks;
     }
-    return totalBrick;
+    
 }
-var result = brickCalculator(38);
-console.log(result);
+var totalFloorBrick = brickCalculator(26);
+console.log(totalFloorBrick);
 
 
 
@@ -51,17 +57,16 @@ console.log(result);
 
 
 //tinyFriend
-// function  tinyFriend(name){
-//    var smallest = name[0];
-//    for(var i = 0; i<name.length; i++){
-//        var str = name[i];
-//        var currentName = str.length;
-//        if(currentName > largest){
-//            smallest = currentName;
-//        }
-//     }
-//     return smallest;
-// }
-// var result = tinyFriend(['kalam', 'salam', 'khadiza', 'ashmaa']);
-// console.log(result);
+function  tinyFriend(names){
+   var shortestName = names[0];
+   for(var i = 0; i<names.length; i++){
+       var currentName = names[i];
+       if(currentName.length < shortestName.length){
+           shortestName = currentName;
+       }
+    }
+    return shortestName;
+}
+var shortestNameFind = tinyFriend(['kalam', 'salman',  'ashmaa', 'diya']);
+console.log(shortestNameFind);
 
